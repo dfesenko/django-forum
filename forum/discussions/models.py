4670b991edcddbc2fea3c64331f0753147e33f55
dfesenko5 <fesenkogithub@gmail.com>
2020-02-22 01:46:53 +0200

    Work on models

--- forum/discussions/models.py
+++ forum/discussions/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+import pgcrypto
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    topics_amount = models.IntegerField(default=0)
+    last_updated_date = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+
+class Topic(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_updated_date = models.DateTimeField(auto_now_add=True, auto_now=True)
+    topic_title = models.CharField(max_length=300)
+    posts_amount = models.IntegerField(default=0)
+    last_active_user = models.ForeignKey(User, default="deleted_user", on_delete=models.SET_DEFAULT)
+
+
+class Post(models.Model):
+    topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    post_body = models.TextField()
+    votes = models.IntegerField(default=0)
+    author_name = models.ForeignKey(User, default="deleted_user", on_delete=models.SET_DEFAULT)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=60)
+    user_password = pgcrypto.EncryptedTextField()
