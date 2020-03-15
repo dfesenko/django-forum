06f43b63c0708846d0ea3a68f6ccf42c79bd06d9
dfesenko5 <fesenkogithub@gmail.com>
2020-03-15 18:58:01 +0200

    Edit user page functionality - finished.

--- forum/discussions/forms.py
+++ forum/discussions/forms.py
@@ -6,3 +6,9 @@ class UserInfoForm(ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileInfoForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['user_location', 'user_about', 'user_avatar']
--- forum/discussions/views.py
+++ forum/discussions/views.py
@@ -11,7 +11,8 @@ from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 
-from .forms import UserInfoForm
+from .forms import UserInfoForm, ProfileInfoForm
+from .models import Profile
 
 
 class IndexView(generic.ListView):
@@ -67,16 +68,40 @@ class UserPageEditView(UserPassesTestMixin, View):
         return allow_access
 
     def get(self, request, *args, **kwargs):
-        user_form = UserInfoForm()
-        return render(request, 'discussions/profile_edit.html', {'user_form': user_form})
+        user_instance = get_object_or_404(User, id=request.user.pk)
+        initial_user_values = {'first_name': user_instance.first_name,
+                               'last_name': user_instance.last_name,
+                               'email': user_instance.email}
+
+        profile_instance = get_object_or_404(Profile, user=request.user.pk)
+        initial_profile_values = {'user_location': profile_instance.user_location,
+                                  'user_about': profile_instance.user_about}
+
+        user_avatar = profile_instance.user_avatar
+
+        user_form = UserInfoForm(initial=initial_user_values)
+        profile_form = ProfileInfoForm(initial=initial_profile_values)
+
+        return render(request, 'discussions/profile_edit.html', {'user_form': user_form,
+                                                                 'profile_form': profile_form,
+                                                                 'user_avatar': user_avatar})
 
     def post(self, request, *args, **kwargs):
         user_instance = get_object_or_404(User, id=request.user.pk)
+        profile_instance = get_object_or_404(Profile, user=request.user.pk)
+
         user_form = UserInfoForm(request.POST, instance=user_instance)
-        if user_form.is_valid():
-            not_empty_fields = [k for k, v in user_form.cleaned_data.items() if v]
-            model_instance = user_form.save(commit=False)
-            model_instance.save(update_fields=not_empty_fields)
+        profile_form = ProfileInfoForm(request.POST, request.FILES, instance=profile_instance)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            not_empty_fields_user = [k for k, v in user_form.cleaned_data.items() if v]
+            user_model_instance = user_form.save(commit=False)
+            user_model_instance.save(update_fields=not_empty_fields_user)
+
+            not_empty_fields_profile = [k for k, v in profile_form.cleaned_data.items() if v]
+            profile_model_instance = profile_form.save(commit=False)
+            profile_model_instance.save(update_fields=not_empty_fields_profile)
+
             return redirect(f'/users/{request.user.pk}/')
 
 
@@ -92,9 +117,3 @@ class UserPasswordResetDoneView(UserPassesTestMixin, PasswordResetDoneView):
 
         allow_access = is_prev_page_was_passw_reset and not self.request.user.is_authenticated
         return allow_access
-
-
-
-
-
-
