0a59809d445c9f9de4bab36ffc6ae30648535e83
dfesenko5 <fesenkogithub@gmail.com>
2020-04-11 21:21:56 +0300

    Refactor signals.py to use F() and avoid race conditions.

--- forum/discussions/signals.py
+++ forum/discussions/signals.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from .models import Profile, Topic, Post
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.db.models import F
+
 
 # create user profile when new user is registered
 @receiver(post_save, sender=User)
@@ -15,15 +17,17 @@ def create_user_profile(sender, instance, created, **kwargs):
 def update_category_when_topic_add(sender, instance, created, **kwargs):
     if created:
         category = instance.category
-        category.topics_amount += 1
+        category.topics_amount = F('topics_amount') + 1
         category.save()
+        category.refresh_from_db()
 
 
 @receiver(post_delete, sender=Topic)
 def update_category_when_topic_delete(sender, instance, **kwargs):
     category = instance.category
-    category.topics_amount -= 1
+    category.topics_amount = F('topics_amount') - 1
     category.save()
+    category.refresh_from_db()
 
 
 # increase/decrease posts counter for topics when new post is added/deleted, update topic's last_updated_date
@@ -31,9 +35,11 @@ def update_category_when_topic_delete(sender, instance, **kwargs):
 def update_topic_when_post_add(sender, instance, created, **kwargs):
     if created:
         topic = instance.topic
-        topic.posts_amount += 1
+        topic.posts_amount = F('posts_amount') + 1
         topic.last_active_user = instance.author
         topic.save()
+        topic.refresh_from_db()
+
         category = topic.category
         category.last_updated_date = instance.creation_date
         category.save()
@@ -42,6 +48,7 @@ def update_topic_when_post_add(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=Post)
 def update_topic_when_post_delete(sender, instance, **kwargs):
     topic = instance.topic
-    topic.posts_amount -= 1
+    topic.posts_amount = F('posts_amount') - 1
     topic.save()
+    topic.refresh_from_db()
 
