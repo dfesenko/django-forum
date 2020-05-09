aceaf17874997566cfaa7938768b83819a6c4e99
dfesenko5 <fesenkogithub@gmail.com>
2020-05-09 20:32:27 +0300

    Add frontend for topic_create.html

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -142,3 +143,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
