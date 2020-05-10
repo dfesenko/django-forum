ea2cdc8c50fd9acce716abcd77d861566615a860
dfesenko5 <fesenkogithub@gmail.com>
2020-05-10 18:06:12 +0300

    Fix: prevent user from signing up with email that is already used by existing registered user

--- forum/registration/forms.py
+++ forum/registration/forms.py
@@ -3,9 +3,20 @@ from django.contrib.auth.models import User
 
 from django.contrib.auth.forms import UserCreationForm
 
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+def validate_duplicate_email(user_email):
+    if user_email in User.objects.all().values_list('email', flat=True):
+        raise ValidationError(
+            _('%(email)s is already used by another registered user. Choose another email.'),
+            params={'email': user_email},
+        )
+
 
 class SignupForm(UserCreationForm):
-    email = EmailField(max_length=200, help_text='Required', required=True)
+    email = EmailField(max_length=200, help_text='Required', required=True, validators=[validate_duplicate_email])
 
     class Meta:
         model = User
