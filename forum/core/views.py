43acb5601c098e29ce99fe8a23bb92eccde76bc2
dfesenko5 <fesenkogithub@gmail.com>
2020-05-10 16:19:29 +0300

    Add frontend for the index page (index.html)

--- forum/core/views.py
+++ forum/core/views.py
@@ -3,6 +3,8 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
+from discussions.models import Topic
+
 
 class IndexView(generic.ListView):
     template_name = 'core/index.html'
@@ -14,6 +16,9 @@ class IndexView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['page_title'] = 'Forum website'
+        context['latest_updated_topics_list'] = Topic.objects.all()[:5]
+        context['new_topics_list'] = Topic.objects.all().order_by('-creation_date')[:5]
+        context['most_popular_topics_list'] = Topic.objects.all().order_by('-posts_amount')[:5]
         return context
 
 
